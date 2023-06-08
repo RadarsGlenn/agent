a15792ded3cb2c8ecffa80dd4d8ef2d9f4c508cd
RadarsGlenn <ribotmael18@gmail.com>
2023-06-08 10:07:48 +0200

    déplacement touche clavier

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,16 @@
 import sys
+import j2l.pytactx.agent as pytactx
+from datetime import datetime
 from datetime import datetime
 from PyQt5 import QtWidgets, uic, QtCore
 from j2l.pytactx.agent import AgentFr
+from PyQt5.QtGui import QKeyEvent
 
 agent = None
+etatFSM1 = "recherche"  # valeurs possibles : "recherche", "poursuite", "veille"
+etatFSMRonde = "gauche"  # valeurs possibles : "gauche", "haut", "droite", "bas"
+xEnnemi, yEnnemi = 0, 0
+debutVeille = datetime.now()
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
@@ -14,14 +21,14 @@ class MainWindow(QtWidgets.QMainWindow):
         self.agent = None
         self.timer = QtCore.QTimer()
         self.timer.setTimerType(QtCore.Qt.PreciseTimer)
-        self.timer.setInterval(20)
+        self.timer.setInterval(100)
         self.timer.timeout.connect(self.onTimerUpdate)
         self.ui = uic.loadUi("formulaire.ui", self)
     
     def onButtonClick(self):
         global agent
-        self.timer.start()
-        self.agent = AgentFr(
+        
+        agent = AgentFr(
             nom=self.nom,
             arene=self.arene,
             username="demo",
@@ -29,6 +36,8 @@ class MainWindow(QtWidgets.QMainWindow):
             url="mqtt.jusdeliens.com",
             verbosite=3
         )
+        self.timer.start()
+
 
     def onNomChange(self, pseudo):
         self.nom = pseudo
@@ -39,32 +48,71 @@ class MainWindow(QtWidgets.QMainWindow):
     def onMdpChange(self, mdp):
         self.mdp = mdp
 
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_Z:
+            self.moveUp()
+        elif event.key() == QtCore.Qt.Key_S:
+            self.moveDown()
+        elif event.key() == QtCore.Qt.Key_D:
+            self.moveRight()
+        elif event.key() == QtCore.Qt.Key_Q:
+            self.moveLeft()
+        elif event.key() == QtCore.Qt.Key_M:
+            self.modeAuto()
+        elif event.key() == QtCore.Qt.Key_0:
+            self.shoot()
+        elif event.key() == QtCore.Qt.Key_5:
+            self.rotationUp()
+        elif event.key() == QtCore.Qt.Key_2:
+            self.rotationDown()
+        elif event.key() == QtCore.Qt.Key_1:
+            self.rotationLeft()
+        elif event.key() == QtCore.Qt.Key_3:
+            self.rotationRight()
+        
+
     def moveUp(self):
         agent.deplacer(0, -1)
+        print("je monte")
+        
 
     def moveDown(self):
         agent.deplacer(0, 1)
+        print("je descends")
 
     def moveLeft(self):
         agent.deplacer(-1, 0)
+        print("je vais à gauche")
 
     def moveRight(self):
         agent.deplacer(1, 0)
+        print("je vais à droite")
 
     def rotationUp(self):
-        agent.orientation = 1
+        agent.orienter(1)
+        print("je tourne vers le haut")
 
     def rotationDown(self):
-        agent.orientation = 3
+        agent.orienter(3)
+        print("je tourne vers le bas")
 
     def rotationLeft(self):
-        agent.orientation = 2
+        agent.orienter(2)
+        print("je tourne vers la gauche")
 
     def rotationRight(self):
-        agent.orientation = 0
+        agent.orienter(0)
+        print("je tourne vers la droite")
     
     def shoot(self):
-        agent.tirer = True
+        agent.tirer(True)
+        print("je tire")
+
+        QtCore.QTimer.singleShot(1000, self.stopShoot)
+
+    def stopShoot(self):
+        agent.tirer(False)
+        print("j'arrête de tirer")
 
     def modeAuto(self):
         pass
